edde5e35998c1b8559c206dd113f2d1bc026df52
Christopher J. Bailey <bailey.cj@gmail.com>
2017-09-05 13:56:59 +0200

    improving contents

--- notebooks/fddhs.py
+++ notebooks/fddhs.py
@@ -14,9 +14,10 @@ def decision_machine(systolic_blood_pressure, weight,
     hearing_threshold (dB)
     
     """    
-    if (norm.cdf(systolic_blood_pressure, 140, 15) > 0.99 and
-        norm.cdf(weight, 70, 10) > 0.99 and
-        norm.cdf(hearing_threshold, 30, 5) > 0.99):
+    if (systolic_blood_pressure > 180 or systolic_blood_pressure < 90 or
+        (norm.cdf(systolic_blood_pressure, 130, 10) > 0.95 and
+         (norm.cdf(weight, 70, 20) > 0.95 or
+          norm.cdf(hearing_threshold, 30, 10) > 0.95))):
         print('You must operate!')
     else:
         print('Send the patient home with some ibuprofen.')
